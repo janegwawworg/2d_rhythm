5d0bf1e116d2a40d2ee1e421c1ac8160eca74700
tiglapiles <tiglapiles@gmail.com>
2021-11-15 16:38:41 +0800

    synchronizing beats

---
+++ Synchronizer.gd
@@ -0,0 +1,32 @@
+extends Node
+
+
+export var bmp := 124
+
+var _bps := 60.0 / bmp
+var _hbps := _bps * 0.5
+var _half_last_beat := 0
+
+onready var _stream := $AudioStreamPlayer2D
+
+
+func _ready() -> void:
+	play_audio()
+
+
+func play_audio() -> void:
+	var time_delay := AudioServer.get_output_latency() + AudioServer.get_time_to_next_mix()
+	yield(get_tree().create_timer(time_delay), "timeout")
+	_stream.play()
+
+
+func _process(delta) -> void:
+	var time: float= (
+		_stream.get_playback_position() + 
+		AudioServer.get_time_since_last_mix() - 
+		AudioServer.get_output_latency()
+	)
+	var half_beat := int(time / _hbps)
+	
+	if half_beat > _half_last_beat:
+		_half_last_beat = half_beat
